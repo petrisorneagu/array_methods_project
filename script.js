175f509e4c6cb2bb64bc080f1cbe404d59fbccef
petrisor <38353376+petrisorneagu@users.noreply.github.com>
2021-02-27 21:25:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+let data = []
+fetRandomUser()
+fetRandomUser()
+fetRandomUser()
+
+
+
+async function fetRandomUser() {
+    const res = await fetch('https://randomuser.me/api')
+    const data = await res.json()
+    const user = data.results[0]
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+    addData(newUser)
+
+}
+
+function addData(obj) {
+    data.push(obj);
+    updateDom()
+}
+
+
+function updateDom(providedData = data) {
+    main.innerHTML = `<h2><strong>Person</strong> Wealth</h2>`;
+    providedData.forEach(item => {
+        const element = document.createElement('div')
+        element.classList.add('person')
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`
+        main.appendChild(element)
+    })
+}
+
+// format money
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+
+//   double money
+function doubleMoney() {
+
+    // console.log('uuu  ', user)
+
+    data = data.map(user => {
+        return { ...user, money: user.money * 2 }
+    })
+    //  console.log(data)
+    updateDom()
+}
+ function sortByRichest(){
+     data.sort((a,b) => b.money - a.money)
+     updateDom()
+ }
+
+//   ev list
+addUserBtn.addEventListener('click', fetRandomUser)
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest)
+// console.log('data   ',data)
